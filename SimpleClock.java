4a56fad03eeadb680d9c3401ce1e43df6d0daf89
Abukarabukare <Abukarshegow@gmail.comm>
2024-04-12 14:09:30 -0400

    Finished

--- SimpleClock.java
+++ SimpleClock.java
@@ -7,7 +7,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.util.*;
+import java.util.List;
 
 
 public class SimpleClock extends JFrame {
@@ -16,18 +17,22 @@ public class SimpleClock extends JFrame {
         SimpleDateFormat timeFormat;
         SimpleDateFormat dayFormat;
         SimpleDateFormat dateFormat;
-    
+
         JLabel timeLabel;
         JLabel dayLabel;
         JLabel dateLabel;
 
         JButton button;
+        JButton button1;
 
 
         String time;
         String day;
         String date;
 
+
+        //I need this
+        TimeZone timeZone;
 
         SimpleClock() {
 
@@ -36,9 +41,13 @@ public class SimpleClock extends JFrame {
             this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             this.setTitle("Digital Clock");
             this.setLayout(new FlowLayout());
-            this.setSize(450, 300);
+            this.setSize(375, 270);
             this.setResizable(false);
-    
+
+            //And this
+            timeZone = TimeZone.getTimeZone("GMT+2");
+
+
             timeFormat = new SimpleDateFormat("hh:mm:ss a");
             dayFormat=new SimpleDateFormat("EEEE");
             dateFormat=new SimpleDateFormat("dd MMMMM, yyyy");
@@ -51,11 +60,47 @@ public class SimpleClock extends JFrame {
             dayLabel.setFont(new Font("Ink Free",Font.BOLD,34));
 
 
+
+            //Button to change to 12/24
             button = new JButton("Change Format");
+            button.setPreferredSize(new Dimension(150, 80));
             button.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    JOptionPane.showMessageDialog(SimpleClock.this ,"WHYYYYY!");
+                    if (timeFormat.toPattern().equals("hh:mm:ss a")) {
+                        timeFormat.applyPattern("HH:mm:ss");
+                    } else {
+                        timeFormat.applyPattern("hh:mm:ss a");
+                    }
+                    time = timeFormat.format(Calendar.getInstance().getTime());
+                    timeLabel.setText(time);
+                }
+            });
+
+
+            //and this inside button
+            //timeFormat.setTimeZone(timeZone);
+            button1 = new JButton("Change To GMT");
+            button1.setPreferredSize(new Dimension(150, 80));
+
+            button1.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (timeFormat.getTimeZone().equals(TimeZone.getDefault())) {
+                        timeFormat.setTimeZone(timeZone);
+                    } else {
+                        timeFormat.setTimeZone(TimeZone.getDefault());
+
+                    }
+                    time = timeFormat.format(Calendar.getInstance().getTime());
+                    timeLabel.setText(time);
+
+
+
+                    //  timeFormat.setTimeZone(timeZone); // Set the time zone for the time format
+                    //        time = timeFormat.format(Calendar.getInstance().getTime()); // Format time with the specified time zone
+                    //        timeLabel.setText(time);
+//                    JOptionPane.showMessageDialog(SimpleClock.this ,"That's Better!");
                 }
             });
 
@@ -65,15 +110,17 @@ public class SimpleClock extends JFrame {
             dateLabel.setFont(new Font("Ink Free",Font.BOLD,30));
 
 
-            this.add(button);
+
             this.add(timeLabel);
             this.add(dayLabel);
             this.add(dateLabel);
+            this.add(button1);
+            this.add(button);
             this.setVisible(true);
-    
+
             setTimer();
         }
-    
+
         public void setTimer() {
             while (Thread.currentThread().isAlive()) {
                 time = timeFormat.format(Calendar.getInstance().getTime());
@@ -96,7 +143,8 @@ public class SimpleClock extends JFrame {
         }
         public static void main(String[] args) {
 
-            new SimpleClock();
+
+          new SimpleClock();
 
         }
     }
